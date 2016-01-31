a008a52727451649c96686c2842e55dc75cf2371
Pasang Sherpa <pasang_sh@hotmail.com>
2016-01-31 14:47:38 -0800

    base box updated to be from 'boxcutter-VAGRANTSLASH-ubuntu1404/2.0.13'

--- README.md
+++ README.md
@@ -13,8 +13,12 @@
 
 3. Alternatively, you can build the box using [packer](https://www.packer.io/intro/getting-started/setup.html): 
     
+    	// base box to build on top of
+    	$ vagrant box add boxcutter/ubuntu1404 --provider virtualbox
+    	// clone the project
         $ git clone git@github.com:pasangsherpa/vagrant-go-webkitgtk.git
         $ cd vagrant-go-webkitgtk
+        // packer build
         $ packer build ubuntu-14.04-amd64.json
         $ vagrant box add go-webkitgtk ubuntu-14.04.virtualbox.box
         $ vagrant init go-webkitgtk
--- scripts/deps.sh
+++ scripts/deps.sh
@@ -1,21 +1,26 @@
 #!/bin/sh -eux
 
 # Install wekitgtk
-apt-get -y install --no-install-recommends -y -q libwebkit2gtk-3.0-dev libgtk-3-dev libcairo2-dev libglib2.0-dev
+apt-get -y install --no-install-recommends -yq \
+	git \
+	libwebkit2gtk-3.0-dev \
+	libgtk-3-dev libcairo2-dev \
+	libglib2.0-dev
 
 # Variables
-go_version="go1.5";
-tar_gz="linux-amd64.tar.gz";
+go_version="go1.5"
+tar_gz="linux-amd64.tar.gz"
 
 # Install golang
-wget https://storage.googleapis.com/golang/${go_version}.${tar_gz} -o /tmp/${go_version}.${tar_gz};
-tar -zxvf ${go_version}.${tar_gz} -C /usr/local;
-rm ${go_version}.${tar_gz};
+wget https://storage.googleapis.com/golang/${go_version}.${tar_gz} -o /tmp/${go_version}.${tar_gz}
+tar -zxvf ${go_version}.${tar_gz} -C /usr/local
+rm ${go_version}.${tar_gz}
 
 # Setup go workspace, see https://golang.org/doc/code.html
-mkdir -p $HOME/go;
-grep '^export GOPATH' $HOME/.bashrc || echo "export GOPATH=$HOME/go" >> $HOME/.bashrc;
-grep '^export PATH' $HOME/.bashrc || echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bashrc;
+mkdir -p $HOME/go
+grep '^export GOPATH' $HOME/.bashrc || echo "export GOPATH=$HOME/go" >> $HOME/.bashrc
+grep '^export PATH' $HOME/.bashrc || echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bashrc
 
 # Install godep, see https://github.com/tools/godep
-GOPATH=$HOME/go /usr/local/go/bin/go get github.com/tools/godep;
+GOPATH=$HOME/go /usr/local/go/bin/go get github.com/tools/godep
+GOPATH=$HOME/go /usr/local/go/bin/go get -u -tags gtk_3_10 github.com/pasangsherpa/webloop/...
