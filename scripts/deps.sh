49e27cb102f0ab1c006accc805fba7a0b98f9207
Pasang Sherpa <pasang_sh@hotmail.com>
2016-02-04 22:42:51 -0800

    example updated

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -40,7 +40,7 @@ script
     XVFBARGS="\$DISPLAY -ac -screen 0 1024x768x16 +extension RANDR"
     PIDFILE="/var/xvfb.pid"
 
-    echo "export DISPLAY=\$DISPLAY" >> /home/vagrant/.bashrc
+    echo "export DISPLAY=\$DISPLAY" >> $HOME/.bashrc
 
     exec >/var/log/upstart/xvfb.log 2>&1
     exec /sbin/start-stop-daemon --start --quiet --pidfile \$PIDFILE --make-pidfile --background --exec \$XVFB -- \$XVFBARGS
